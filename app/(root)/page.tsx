723b2d3c382b4facf0962559d2b962bbe67389f9
Shivam <shivamk200227@gmail.com>
2024-04-14 22:55:00 +0530

    background modified and made some necessary change

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,23 @@
+import {
+	BackgroundImage1,
+	BackgroundImage2,
+} from '@/components/quoteGenerator/quote-generatror-elements'
+
 export default function Home() {
-	return <main></main>
+	return (
+		<main>
+			<BackgroundImage1
+				src={'/assets/images/cloud-and-thunder.png'}
+				height={300}
+				width={300}
+				alt="Cloud and thunder"
+			/>
+			<BackgroundImage2
+				src={'/assets/images/cloudy-weather.png'}
+				height={300}
+				width={640}
+				alt="Cloud and thunder"
+			/>
+		</main>
+	)
 }
--- components/quoteGenerator/quote-generatror-elements.tsx
+++ components/quoteGenerator/quote-generatror-elements.tsx
@@ -1,9 +1,13 @@
 'use client'
+import { useEffect, useState } from 'react'
 import styled from 'styled-components'
-import Link from 'next/link'
 import Image from 'next/image'
 
-export const GradientBackground = styled.div`
+interface GradientBackgroundProps {
+	children: React.ReactNode
+}
+
+const GradientBackgroundWrapper = styled.div`
 	background: linear-gradient(to right, #000046, #1cb5e0);
 	background-size: 400% 400%;
 	animation: gradient 8s ease infinite;
@@ -20,4 +24,31 @@ export const GradientBackground = styled.div`
 			background-position: 0% 50%;
 		}
 	}
+`
+
+export const GradientBackground = ({ children }: GradientBackgroundProps) => {
+	const [isMounted, setIsMounted] = useState(false)
+
+	useEffect(() => {
+		setIsMounted(true)
+	}, [])
+
+	return (
+		<GradientBackgroundWrapper>
+			{isMounted && children}
+		</GradientBackgroundWrapper>
+	)
+}
+
+export const BackgroundImage1 = styled(Image)`
+	position: relative;
+	z-index: 1;
+	margin-left: -10px;
+	margin-top: -10px;
+`
+export const BackgroundImage2 = styled(Image)`
+	position: fixed;
+	z-index: 1;
+	right: -120px;
+	bottom: -10px;
 `
