7a97b4d3b659eb45aadf164380819e3159877990
Shivam <shivamk200227@gmail.com>
2024-05-09 13:23:47 +0530

    quote generator ui done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,49 @@
+'use client'
+
+
+import { useState } from 'react'
 import {
 	BackgroundImage1,
 	BackgroundImage2,
+	FooterContainer,
+	FooterLink,
+	GenerateQuoteButton,
+	GenerateQuoteButtonText,
+	QuoteGeneratorContainer,
+	QuoteGeneratorInnerContainer,
+	QuoteGeneratorSubTitle,
+	QuoteGeneratorTitle,
 } from '@/components/quoteGenerator/quote-generatror-elements'
 
 export default function Home() {
+	const [numberOfQuotes, setNumberOfQuotes] = useState<number | null>(0)
+
 	return (
 		<main>
+			{/* QuoteGeneratorModal */}
+			{/* <QuoteGeneratorModal /> */}
+
+			{/* Quote Generator */}
+
+			<QuoteGeneratorContainer>
+				<QuoteGeneratorInnerContainer>
+					<QuoteGeneratorTitle>Quote Generator</QuoteGeneratorTitle>
+					<QuoteGeneratorSubTitle>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit.
+						Aperiam minus et esse similique perspiciatis maxime
+						voluptate ipsam molestias. Itaque ea temporibus, cum
+						vitae consectetur fugiat ad quis amet blanditiis nisi
+						excepturi dolore alias.
+					</QuoteGeneratorSubTitle>
+					<GenerateQuoteButton onClick={() => {}}>
+						<GenerateQuoteButtonText>
+							Make a Quote
+						</GenerateQuoteButtonText>
+					</GenerateQuoteButton>
+				</QuoteGeneratorInnerContainer>
+			</QuoteGeneratorContainer>
+
+			{/* background images */}
 			<BackgroundImage1
 				src={'/assets/images/cloud-and-thunder.png'}
 				height={300}
@@ -18,6 +56,20 @@ export default function Home() {
 				width={640}
 				alt="Cloud and thunder"
 			/>
+
+			{/* footer */}
+			<FooterContainer>
+				Quotes Generated {numberOfQuotes}
+				<br />
+				Developed with POWER IN HIS HANDS by{' '}
+				<FooterLink
+					href={'https://www.instagram.com/shivam_27k/'}
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					@shivam27k
+				</FooterLink>
+			</FooterContainer>
 		</main>
 	)
 }
--- components/quoteGenerator/quote-generatror-elements.tsx
+++ components/quoteGenerator/quote-generatror-elements.tsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import Image from 'next/image'
+import Link from 'next/link'
 
 interface GradientBackgroundProps {
 	children: React.ReactNode
@@ -51,4 +52,111 @@ export const BackgroundImage2 = styled(Image)`
 	z-index: 1;
 	right: -120px;
 	bottom: -10px;
+`
+
+export const FooterContainer = styled.div`
+	width: 100vw;
+	text-align: center;
+	font-size: 1.5rem;
+	font-family: 'Source Code Pro', sans-serif;
+	position: absolute;
+	bottom: 0;
+	color: #fff;
+	z-index: 99999;
+	margin-bottom: 10px;
+`
+export const FooterLink = styled(Link)`
+	color: #fff;
+`
+
+export const QuoteGeneratorContainer = styled.div`
+	min-height: 350px;
+	min-width: 350px;
+	height: 70vh;
+	width: 70vw;
+	border: 2px solid #ffffff22;
+	border-radius: 15px;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	position: absolute;
+	z-index: 2;
+
+	background: rgba(0, 0, 70, 0.3);
+	box-shadow: 0 8px 32px rgba(31, 38, 135, 0.37);
+	backdrop-filter: blur(20px);
+	-webkit-backdrop-filter: blur(20px);
+	border-radius: 10px;
+	border: 1px solid rgba(255, 255, 255, 0.18);
+`
+export const QuoteGeneratorInnerContainer = styled.div`
+	width: 100%;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	position: absolute;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+`
+export const QuoteGeneratorTitle = styled.div`
+	font-family: 'Permanent Marker', cursive;
+	font-size: 50px;
+	text-align: center;
+	color: #fff;
+	padding: 0px 20px 0px 20px;
+	position: relative;
+	@media only screen and (max-width: 600px) {
+		font-size: 30px;
+	}
+`
+export const QuoteGeneratorSubTitle = styled.div`
+	color: #fff;
+	font-family: 'Caveat', cursive;
+	font-size: 35px;
+	position: relative;
+	width: 100%;
+	text-align: center;
+	padding: 0px 20px 0px 20px;
+	@media only screen and (max-width: 600px) {
+		font-size: 25px;
+	}
+`
+export const GenerateQuoteButton = styled.button`
+	height: 100px;
+	width: 300px;
+	border: 2px solid darkgrey;
+	border-radius: 20px;
+
+	margin-top: 20px;
+	position: relative;
+	transition: 0.2s all ease-in-out;
+	cursor: pointer;
+	top: 20px;
+	margin: auto;
+	transform-origin: center;
+
+	background: rgba(0, 0, 70, 0.3);
+	box-shadow: 0 8px 32px rgba(31, 38, 135, 0.37);
+	backdrop-filter: blur(20px);
+	-webkit-backdrop-filter: blur(20px);
+	border-radius: 10px;
+	border: 1px solid rgba(255, 255, 255, 0.18);
+	&:hover {
+		filter: brightness(2);
+		transition: 0.2s all ease-in-out;
+		transform: scale(1.1);
+		transform-origin: center;
+	}
+`
+export const GenerateQuoteButtonText = styled.span`
+	color: #fff;
+	font-family: 'Caveat', cursive;
+	font-size: 35px;
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	text-align: center;
+	width: 100%;
 `
