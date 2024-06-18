9de2b2a6a8af150bbb9b131e837e949c48d97abd
Shivam <shivamk200227@gmail.com>
2024-06-18 12:06:53 +0530

    configured AWS(api, function auth)

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,29 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "quotegenerator": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "quotegenerator88242dfa": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "generateAQuote": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,20 @@
 import type { Metadata } from 'next'
 import '../globals.css'
 import { GradientBackground } from '@/components/quoteGenerator/quote-generatror-elements'
+import { Amplify } from 'aws-amplify'
+import awsmobile from '../../src/aws-exports'
+import { ResourcesConfig } from 'aws-amplify'
+
+interface CustomAmplifyConfig extends ResourcesConfig {
+	ssr?: boolean
+}
+
+const customConfig: CustomAmplifyConfig = {
+	...awsmobile,
+	ssr: true,
+}
+
+Amplify.configure(customConfig)
 
 export const metadata: Metadata = {
 	title: 'QuoteFek',
